3a1bbc24883ac37f703479d0b98ad09cda13cb48
krzysztofee <gitaragra@gmail.com>
2024-04-06 22:23:56 +0200

    Add settings folder

---
+++ src/pages/settings/indexSettings.ts
@@ -0,0 +1,8 @@
+const main = document.getElementById("main")
+
+const div = document.createElement("div")
+div.innerText = "llllllll"
+div.classList.add("text-red-900", "m-5");
+main?.append(div)
+
+console.log('',main)
